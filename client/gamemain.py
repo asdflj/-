e1b7ef57bd05edda312072bd133f8badd5dec640
asdflj <33802162+asdflj@users.noreply.github.com>
2018-06-27 17:58:13 +0800

    add

--- client/gamemain.py
+++ client/gamemain.py
@@ -3,6 +3,8 @@ from settings import Settings
 import sys
 from gamefunction import *
 import math
+from threading import Thread
+import time
 
 class Main:
     def __init__(self,user):
@@ -13,7 +15,7 @@ class Main:
         self.packge = self.loadPackge()  # 加载资源包
         pygame.display.set_caption('斗地主') #设置标题
         pygame.event.set_blocked(pygame.MOUSEMOTION | pygame.MOUSEBUTTONDOWN | pygame.QUIT) #设置监听事件 测试用
-        self.events = [] #要监听的事件
+        self.events = {'touch':[],'display':[]} #要监听的事件
         #初始化要绘制的信息
 
         self.mySelf = self.drawSelf()  # 绘制自己
@@ -21,34 +23,96 @@ class Main:
         self.rightUser = self.drawRightUser() #绘制右边玩家
         self.mouse = Mouse(self.screen) #鼠标点击点绘制 用来判断精灵是否碰撞
 
+        self.t =Thread(target=self.getMessage)
+        self.t.setDaemon(True)
+        self.t.start()
+
     def drawBackGroundImage(self):
         '''绘制背景图片'''
         self.screen.blit(self.packge.backgroundImg, (0, -24))  # 绘制背景图片
 
-    def eventsAppend(self,spritesGroup,fun,delete=True):
+    def getMessage(self):
+        while True:
+            # msg = self.user.getMessage(True)
+            time.sleep(1)
+            msg={'data':'选地主','title':'选地主'}
+            if msg['title'] =='选地主':
+                self.appendDisplayEvents(self.mySelf.selectDiZhu)
+                self.appendTouchEvents(self.mySelf.getPointGroup(),self.getPoint,True)
+            elif msg['title'] =='显示牌':
+                self.appendDisplayEvents(self.showPlayerCard)
+                pass
+            elif msg['title'] =='出牌':
+                self.appendDisplayEvents(self.mySelf.showCard,self.packge.poker[:10])
+                self.appendDisplayEvents(self.mySelf.pushCard)
+                self.appendTouchEvents(self.mySelf.getPokerGroup(),self.popPoker,True)
+                self.appendTouchEvents(self.mySelf.getButtonGroup(),self.pushCard,True)
+            elif msg['title'] =='结束':
+                self.appendDisplayEvents(self.playerWin,'xx Win')
+
+    # def showPlayerCard(self,data):
+    #     #解析
+    def playerWin(self,msg):
+        font = self.packge.font  # 字体
+        text = font.render('%s'%msg, True, (0,0,0))  #显示剩余多少张牌
+        self.screen.blit(text,(400,300)) #绘制到屏幕上
+
+    def pushCard(self,sprite):
+        poker = []
+        for i in self.packge.poker:
+            if i.pop:
+                poker.append(i.ID)
+        print(poker)
+        #转换为对应的列表发送出去
+
+    def getPoint(self,sprite):
+        point = sprite.point
+        print(point)
+        # if point == 0:
+        #     msg =self.user.convert('point','Y')
+        # else:
+        #     msg = self.user.convert('point', 'N')
+        # self.user.sendMessage(msg)
+
+    def popPoker(self,sprite):
+        if sprite.pop ==True:
+            sprite.pop =False
+        else:
+            sprite.pop = True
+
+    def appendTouchEvents(self,spritesGroup,fun,delete=True):
         '''spritesGroup = 精灵组 fun = 要执行的函数 delete事件执行完后是否删除 会将相撞的精灵对象放入到函数中，
         使用函数进行处理即可'''
-
         def append():
             rect = pygame.sprite.spritecollideany(self.mouse,spritesGroup)
             for sprite in spritesGroup.sprites():
                 if rect == sprite.rect:
                     fun(sprite)
             return delete
-        return append
+        self.events['touch'].append(append)
+
+    def appendDisplayEvents(self,fn,data=None):
+        def append():
+            if data == None:
+                fn()
+            else:
+                fn(data)
+        self.events['display'].append(append)
 
     def gameEvent(self):
         '''处理事件
            请使用装饰器进行重写
         '''
+        for event in self.events['display']:
+            event()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 self.mouse.drawPoint()
-                for i in self.events:
+                for i in self.events['touch']:
                     if i():
-                        del self.events[self.events.index(i)]
+                        del self.events['touch'][self.events['touch'].index(i)]
 
     # 示例
     #         if i.pop == False:
@@ -67,10 +131,10 @@ class Main:
         while True: #主事件循环
             self.drawBackGroundImage() #背景覆盖
             # self.events.append(self.eventsAppend(self.mySelf.getPokerGroup(),self.fn,True)) #事件添加
-            self.drawOutPokerArea(self.packge.poker[:20])  # 绘制出牌区域
-            self.mySelf.showCard(self.packge.poker[:10])  # 绘制手牌
-            self.rightUser.showCard((self.packge.poker[1:])) # 绘制手牌
-            self.leftUser.showCard((self.packge.poker[1:]))  # 绘制手牌
+            # self.drawOutPokerArea(self.packge.poker[:20])  # 绘制出牌区域
+            # self.mySelf.showCard(self.packge.poker[:10])  # 绘制手牌
+            # self.rightUser.showCard((self.packge.poker[1:])) # 绘制手牌
+            # self.leftUser.showCard((self.packge.poker[1:]))  # 绘制手牌
             # self.mySelf.pushCard()
             # self.mySelf.selectDiZhu()
             # self.rightUser.selectDiZhu()
@@ -127,7 +191,7 @@ class Main:
                 self.cardBackGround = pygame.image.load(self.settings.cardBackGround)  # 加载卡牌背景
                 self.cardBackGround = pygame.transform.scale(self.cardBackGround, (40, 50))  # 缩放提示牌大小
                 point = [self.settings.point0,self.settings.point1,self.settings.point2,self.settings.point3]
-                self.points = [Point(point[x],x) for x in range(4)] # 加载叫分图片
+                self.points = [Point(point[x],x) for x in range(2)] # 加载叫分图片
                 self.pass1 = Buttons(self.settings.pass1,600,360,False) #过图片 精灵类
                 self.put = Buttons(self.settings.put,300,360,True)  #出牌 精灵类
                 self.putError = pygame.image.load(self.settings.putError) #出牌错误 图片 未用到
--- game_room.py
+++ game_room.py
@@ -20,35 +20,35 @@ class Game:
         # t1.setDaemon(True)
         # t1.start()  #改为游戏主流程来控制消息接收
 
-    #接收消息并发给处理函数
-    def getMessage(self):
-        #初始化监听列表
-        rlist = []
-        wlist = []
-        xlist = []
-        for user in self.users:
-            rlist.append(user.getSockfd())
-        while True:
-            #如果没有满人就尝试加入新的监听套接字
-            if len(rlist) !=3:
-                rlist = []
-                for user in self.users:
-                    rlist.append(user.getSockfd())
-            try:
-                rs, ws, xs = select(rlist, wlist, xlist)
-                user = self.getUser(rs)  #获取接收到的套接字属于哪个用户
-                msg = eval(user.getMessage().decode())  #接收并处理消息
-                self.handler(user,msg,rlist) #处理消息
-            except (BrokenPipeError,AttributeError): #玩家退出 套接字连接失效
-                self.playerExit(user,rlist)  #处理玩家退出信息
-        time.sleep(0.1)
+    # #接收消息并发给处理函数
+    # def getMessage(self):
+    #     #初始化监听列表
+    #     rlist = []
+    #     wlist = []
+    #     xlist = []
+    #     for user in self.users:
+    #         rlist.append(user.getSockfd())
+    #     while True:
+    #         #如果没有满人就尝试加入新的监听套接字
+    #         if len(rlist) !=3:
+    #             rlist = []
+    #             for user in self.users:
+    #                 rlist.append(user.getSockfd())
+    #         try:
+    #             rs, ws, xs = select(rlist, wlist, xlist)
+    #             user = self.getUser(rs)  #获取接收到的套接字属于哪个用户
+    #             msg = eval(user.getMessage().decode())  #接收并处理消息
+    #             self.handler(user,msg,rlist) #处理消息
+    #         except (BrokenPipeError,AttributeError): #玩家退出 套接字连接失效
+    #             self.playerExit(user,rlist)  #处理玩家退出信息
+    #     time.sleep(0.1)
 
-    def playerExit(self,user,rlist):
+    def playerExit(self,user):
         getUserIndex = self.users.index(user)
         self.userOut.send(user.fileno)  #发送给主进程用户信息使其删除该用户
         del self.users[getUserIndex]
-        del rlist[rlist.index(user.getSockfd())]  #清理信息
-        self.sendRoomMessage(str(getUserIndex),'PlayOut' )  #告知其他玩家该用户已退出
+        # del rlist[rlist.index(user.getSockfd())]  #清理信息
+        # self.sendRoomMessage(str(getUserIndex),'PlayOut' )  #告知其他玩家该用户已退出
 
     #等待用户加入
     def waitUserJoin(self):
@@ -56,20 +56,20 @@ class Game:
             user = self.userJoin.recv()
             self.users.append(user)
 
-    #获取接收到的套接字属于哪个用户
-    def getUser(self,rlist):
-        for user in self.users:
-            for i in rlist:
-                if user.getSockfd() is i:
-                    return user
+    # #获取接收到的套接字属于哪个用户
+    # def getUser(self,rlist):
+    #     for user in self.users:
+    #         for i in rlist:
+    #             if user.getSockfd() is i:
+    #                 return user
 
-    #处理接收到的信息
-    def handler(self,user,msg,rlist):
-        if msg['title'] == 'msg':
-            self.sendRoomMessage(msg['data'],'msg',user)
-        elif msg['title'] == 'loginOut':
-            self.playerExit(user, rlist)
-            user.closeSockfd()
+    # #处理接收到的信息
+    # def handler(self,user,msg,rlist):
+    #     if msg['title'] == 'msg':
+    #         self.sendRoomMessage(msg['data'],'msg',user)
+    #     elif msg['title'] == 'loginOut':
+    #         self.playerExit(user, rlist)
+    #         user.closeSockfd()
 
     #发送消息给其他人
     def sendRoomMessage(self,msg,title,user=None):
@@ -79,24 +79,8 @@ class Game:
                 i.sendMessage(send_msg)
 
     def startGame(self):
-            self.sendRoomMessage('开始', 'msg')
+        try:
+            # self.sendRoomMessage('开始', 'msg')
             game_main.main(self)
-
-            # while 1:
-            #     pass
-            # 执行游戏流程
-            # 将发牌导出到列表
-            # paly1, paly2, paly3, dipai = fapai()
-            # paly1_fh = paixu(paly1)
-            # paly2_fh = paixu(paly2)
-            # paly3_fh = paixu(paly3)
-            # dipai_fh = paixu(dipai)
-            # self.users[0].sendMessage(self.users[0].convert(repr(paly1_fh), 'puker'))
-            # self.users[1].sendMessage(self.users[1].convert(repr(paly2_fh), 'puker'))
-            # self.users[2].sendMessage(self.users[2].convert(repr(paly3_fh), 'puker'))
-            self.process()   #流程循环迭代
-
-    def process(self):  #方案1使用迭代的方式来执行游戏的进程
-        pass
-    # if not user.puker:
-    #     return
+        except Exception as e:
+            self.playerExit(e)
